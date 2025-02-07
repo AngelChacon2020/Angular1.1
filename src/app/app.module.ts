d1963af97e8aa5c8549cd8d56bd743331712efd3
Angel Chacon <angel1998chacon@gmail.com>
2025-02-06 22:06:18 -0600

    Inicio nueva seccion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,21 @@ import { HeroComponent } from './heroes/hero/hero.component';
 import { ListComponent } from './heroes/list/list.component';
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CounterModule,
-    HeroesModule
-
+    HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dbz-min-page',
+  templateUrl: './main-page.component.html'
+})
+
+export class MainPageComponent  {
+
+}
